7fd174684e97b1d86ca60079c099a3e8dc8b232c
clementthomas <clement@cotyl.net>
2024-03-06 13:37:19 +0100

    debug

--- event_track_calendar_event/models/event_registration.py
+++ event_track_calendar_event/models/event_registration.py
@@ -1,15 +1,18 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import fields, models, api, Command
-
+import logging
+_logger = logging.getLogger(__name__)
 
 class EventRegistration(models.Model):
     _inherit = "event.registration"
     
     def write(self, vals):
+        _logger.warning("call write...")
         res = super().write(vals)        
         for registration in self:
             for track in registration.event_id.track_ids:
                 track.sync_calendar_event()
+        _logger.warning("called !")
         return res
 
     @api.model_create_multi
