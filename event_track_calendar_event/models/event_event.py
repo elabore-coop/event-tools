9bc61c083fa4b9f619dae735704f9b142c544e7e
clementthomas <clement@cotyl.net>
2024-03-06 13:32:18 +0100

    debug

--- event_track_calendar_event/models/event_event.py
+++ event_track_calendar_event/models/event_event.py
@@ -1,5 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import fields, models, api, Command
+import logging
 _logger = logging.getLogger(__name__)
 
 class EventEvent(models.Model):
