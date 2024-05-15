756f907ac8447076ec95b0f06e66308174c1a273
clementthomas <clement@cotyl.net>
2024-05-15 10:19:13 +0200

    [NEW] event_sale_configurator_optionnal

--- event_sale_configurator_optionnal/__manifest__.py
+++ event_sale_configurator_optionnal/__manifest__.py
@@ -1,15 +1,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Event speakers",
+    "name": "event_sale_configurator_optionnal",
     "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Elabore",
     "website": "https://www.elabore.coop",
     "category": "",
-    "depends": ["website_event_track"],
+    "depends": ["event_sale"],
     "data": [        
-        'views/event_track_views.xml',         
+        'wizard/event_configurator_views.xml',         
     ],    
     "installable": True,
 }
